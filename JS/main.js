c02cd76b9a032afb4358a1a18001cecf093dad1b
Arthur Paiva <arthur-68@hotmail.com>
2026-07-03 22:06:27 -0300

    Alterando elementos no JS, Index e CSS.

--- JS/main.js
+++ JS/main.js
@@ -1,23 +1,44 @@
-const container = document.getElementById("personagens");
+const conteudo = document.getElementById("conteudo");
+const personagens = document.getElementById("personagens");
+const vilas = document.getElementById("vilas");
+personagens.addEventListener("click", getPersonagem);
+vilas.addEventListener("click", getVilas);
 
+function getPersonagem() {
     fetch("https://dattebayo-api.onrender.com/characters")
         .then(response => response.json())
         .then(data => {
+            conteudo.innerHTML = "";
 
-             container.innerHTML = "";
-            
-             data.characters.forEach(personagem =>{
-                container.innerHTML += `
-                 <div class="card">
-                    <img src="${personagem.images[0]}" alt="${personagem.name}">
-                    <p class="titulo">${personagem.name}</p>
-                    <p class="debut">
-                        ${personagem.debut.manga}; 
-                    </p> 
-                </div>
+            data.characters.forEach(personagem => {
+                conteudo.innerHTML += `
+                    <div class="card">
+                        <img src="${personagem.images[0]}" alt="${personagem.name}">
+                        <p>${personagem.name}</p>
+                        <p>${personagem.debut.manga}</p>
+                        
+                    </div>
                 `;
-                // o ponto(.) se refere a sequência da primeira informação(debut)
-        });
+            });
+        })
+        .catch(error => console.log(error));
+}
 
-    })
-    .catch(error => console.log(error));
+function getVilas() {    
+    fetch("https://dattebayo-api.onrender.com/villages")
+        .then(response => response.json())
+        .then(data => {
+            conteudo.innerHTML = "";
+
+            data.villages.forEach(vila => {
+                conteudo.innerHTML += `
+                    <div class="card">
+                        <p>${vila.id}</p>
+                        <p>${vila.name}</p>                        
+                    </div>
+                `;
+                console.log(vila.name);
+            });
+        })
+        .catch(error => console.log(error));
+}
